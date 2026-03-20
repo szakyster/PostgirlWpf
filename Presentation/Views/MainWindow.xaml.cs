b16f70e387f814837a90d0521d2be147f368ffba
szakyster <szakyster@gmail.com>
2026-03-20 14:21:59 +0100

    add accordion

--- Presentation/Views/MainWindow.xaml.cs
+++ Presentation/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Controls;
 using Postgirl.Presentation.ViewModels;
 using Postgirl.Services;
 
@@ -6,12 +7,27 @@ namespace Postgirl.Presentation.Views
 {
     public partial class MainWindow : Window
     {
+        private readonly Expander[] _sidebarExpanders;
 
         public MainWindow(MainViewModel vm)
         {
             InitializeComponent(); 
             DataContext = vm;
             Closing += OnClosing;
+
+            _sidebarExpanders = [SavedExpander, HistoryExpander];
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Expander expanded) return;
+            if (_sidebarExpanders is null) return;
+
+            foreach (var expander in _sidebarExpanders)
+            {
+                if (expander != expanded)
+                    expander.IsExpanded = false;
+            }
         }
 
         private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
