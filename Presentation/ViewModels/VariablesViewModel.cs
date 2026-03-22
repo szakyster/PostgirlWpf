8d2d06a7939b8373fe393e1b4d0a015e86aed4c8
szakyster <szakyster@gmail.com>
2026-03-22 19:39:52 +0100

    duplication alert

--- Presentation/ViewModels/VariablesViewModel.cs
+++ Presentation/ViewModels/VariablesViewModel.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 using Postgirl.Common;
 using Postgirl.Domain.Variables;
@@ -17,7 +21,10 @@ public class VariablesViewModel : BaseViewModel
         _variablesService = variablesService;
 
         foreach (var entry in variablesService.Items)
-            Items.Add(new VariableItemViewModel(entry));
+            AddItem(new VariableItemViewModel(entry));
+
+        Items.CollectionChanged += OnItemsCollectionChanged;
+        RefreshDuplicates();
 
         AddCommand = new RelayCommand(() =>
         {
@@ -36,4 +43,42 @@ public class VariablesViewModel : BaseViewModel
 
     public ICommand AddCommand { get; }
     public ICommand DeleteCommand { get; }
+
+    private void AddItem(VariableItemViewModel vm)
+    {
+        vm.PropertyChanged += OnItemPropertyChanged;
+        Items.Add(vm);
+    }
+
+    private void OnItemsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems != null)
+            foreach (VariableItemViewModel vm in e.NewItems)
+                vm.PropertyChanged += OnItemPropertyChanged;
+
+        if (e.OldItems != null)
+            foreach (VariableItemViewModel vm in e.OldItems)
+                vm.PropertyChanged -= OnItemPropertyChanged;
+
+        RefreshDuplicates();
+    }
+
+    private void OnItemPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(VariableItemViewModel.Key))
+            RefreshDuplicates();
+    }
+
+    private void RefreshDuplicates()
+    {
+        var duplicateKeys = Items
+            .Where(vm => !string.IsNullOrEmpty(vm.Key))
+            .GroupBy(vm => vm.Key, StringComparer.Ordinal)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key)
+            .ToHashSet(StringComparer.Ordinal);
+
+        foreach (var vm in Items)
+            vm.IsDuplicate = duplicateKeys.Contains(vm.Key);
+    }
 }
