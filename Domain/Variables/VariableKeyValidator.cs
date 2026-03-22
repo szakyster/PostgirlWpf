498eca0ebf68a8b95793c0a981f2378de677bc9e
szakyster <szakyster@gmail.com>
2026-03-22 17:20:05 +0100

    extend key validation

--- Domain/Variables/VariableKeyValidator.cs
+++ Domain/Variables/VariableKeyValidator.cs
@@ -5,7 +5,7 @@ namespace Postgirl.Domain.Variables;
 public static class VariableKeyValidator
 {
     // Allows: letters, digits, underscore, hyphen, dot. Rejects: spaces, tabs, parentheses, etc.
-    private static readonly Regex ValidPattern = new(@"^[\w\-\.]*$", RegexOptions.Compiled);
+    private static readonly Regex ValidPattern = new(@"^[\w\-\.]{0,256}$", RegexOptions.Compiled);
 
     public static bool IsValid(string key) => ValidPattern.IsMatch(key);
 }
